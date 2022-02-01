1c127017afacc31e7b197a0883f3e61af6c5d869
MrclHerrera <mrclherrera@hotmail.com>
2022-02-01 18:00:39 -0300

    aula3

---
+++ Comentario.js
@@ -0,0 +1,10 @@
+//console.log para exibir algo no tela do computador!
+console.log("Passo #1");
+console.log("Passo #2"); // mais um exemplo de console.log
+console.log("Passo #3");
+
+/*
+
+Comentario de varias linhas !!!
+
+*/
