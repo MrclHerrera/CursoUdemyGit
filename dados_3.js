0f7f228dff27c8e972065a52d25bb208cd10af5b
MrclHerrera <mrclherrera@hotmail.com>
2022-02-02 14:13:45 -0300

    Conclui Fundamentos JS

---
+++ dados_3.js
@@ -0,0 +1,11 @@
+var idade = 31;
+console.log(typeof 31);
+
+var salario = 4578.23;
+console.log(typeof salario);
+
+var local = "Casa";
+console.log(typeof local);
+
+var estaChovendo = false;
+console.log(typeof estaChovendo);
---
+++ desafioArea.js
@@ -0,0 +1,8 @@
+const PI = 3.1415;
+var raio = 10;
+var resultado = PI * raio * raio ;
+
+console.log("O resultado da area é :  " + resultado + " m2 ");
+console.log(Math.PI);
+
+
